074f395bffd88b6b073e2111345fb563e2fa57af
Tlacaelel <radeon777@live.com>
2021-04-27 11:19:17 -0600

    Added file loader and url loaders

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./styles.css";
+import Image from "./image.png";
+import Logo from './logo.svg';
 
 export default function app() {
   return (
-    <h1>React Typescript webpack starter template</h1>
-  )
+    <>
+      <h1>React Typescript webpack starter template</h1>
+      <img src={Image} alt="imagen" width="300" />
+      <img src={Logo} alt="imagen" width="300" />
+
+    </>
+  );
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -16,6 +16,17 @@ module.exports = {
             loader: 'babel-loader',
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use:['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },{
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline'
       }
     ]
   },
