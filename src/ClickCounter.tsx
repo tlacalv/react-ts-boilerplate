6c3da2718913fe5e1215af1a68132c4ab86c77b3
Tlacaelel <radeon777@live.com>
2021-05-05 20:39:08 -0600

    added react refresh webpack plugin

---
+++ src/ClickCounter.tsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+
+export default function ClickCounter() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <button onClick={()=>setCount(c=>c+1)}>Count: {count}</button>
+    </div>
+  )
+}
--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import "./styles.css";
 import Image from "./image.png";
 import Logo from './logo.svg';
+import ClickCounter from './ClickCounter';
 
 export default function app() {
   return (
     <>
-      <h1>React Typescript webpack starter template - {process.env.NODE_ENV} {process.env.name}</h1>
+      <h1>Reactd Typescript webpack starter template - {process.env.NODE_ENV} {process.env.name}</h1>
       <img src={Image} alt="imagen" width="300" />
       <img src={Logo} alt="imagen" width="300" />
-
+      <ClickCounter />
     </>
   );
 }
